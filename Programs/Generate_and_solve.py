ddc75552305371ca902ee8fb2e6422417b73a3fb
crisp-macaroon <adhwaithbpillai@gmail.com>
2024-12-07 12:57:05 +0530

    nice code

--- Programs/Generate_and_solve.py
+++ Programs/Generate_and_solve.py
@@ -2,6 +2,7 @@ import pygame
 import random as rd
 import networkx
 import miscFunc as mf
+import traversals as tr
 
 RES = WIDTH, HEIGHT = 800, 800
 TILE,ALGORITHM = mf.get_tile_size_and_algorithm()
@@ -58,7 +59,7 @@ class Cell:
         if left and not left.visited:
             neighbors.append(left)
         return rd.choice(neighbors) if neighbors else None
-
+    
 def remove_walls(node, next):
     dx = node.x - next.x
     if dx == 1:
@@ -104,163 +105,58 @@ def draw_path(path,color):
     pygame.display.flip()
     pygame.time.delay(DELAY)
 
-def heuristic(a, b):    #manhattan distance
-    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+def maze_generator_dfs(grid_cells):
+    current_cell = grid_cells[0]
+    stack = []
+    
+    while True:
+        [cell.show() for cell in grid_cells]
+        current_cell.visited = True
+        current_cell.current_cell()
 
-def a_star(graph, start, goal):
-    open_set = {start}  # The set of nodes to be evaluated
-    pq = [(0, start)]
-
-    g_score = {node: float('inf') for node in graph.nodes}  # Cost from start along best known path.
-    g_score[start] = 0
-
-    f_score = {node: float('inf') for node in graph.nodes} # Estimated total cost from start to goal through y.
-    f_score[start] = heuristic(start, goal)
-
-    parent = {}
-    path = []
-
-    while pq:
-        _, node = pq.pop(0)
-
-        if node == goal:
-            path = []
-            while node in parent:
-                prev = parent[node]
-                path.append((prev, node))
-                node = prev
-            path.reverse()
-            return path
-
-        open_set.remove(node)   # Remove the node from the open set
-
-        for neighbor in graph.neighbors(node):
-            tentative_g_score = g_score[node] + 1   # Assuming the weight of each edge is 1
-
-            if tentative_g_score < g_score[neighbor]:
-                parent[neighbor] = node
-                g_score[neighbor] = tentative_g_score
-                f_score[neighbor] = g_score[neighbor] + heuristic(neighbor, goal)   # f(x) = g(x) + h(x)
-
-                if neighbor not in open_set:
-                    open_set.add(neighbor)
-                    pq.append((f_score[neighbor], neighbor))
-                    pq.sort()                                   # Sort the priority queue based on f_score
-                    draw_path([(node, neighbor)], 'green')
-
-    return []
-
-def dfs_traversal(graph, start, goal):
-    stack = [start]
-    visited = set()
-    parent = {}
-    path_set = []
-
-    while stack:
-        node = stack.pop()
-
-        if node not in visited:
-            visited.add(node)
-
-            if node in parent:
-                path_set.append((parent[node], node))
-                draw_path([(parent[node], node)], 'green')
-
-            if node == goal:
-                path = []
-                while node in parent:
-                    prev = parent[node]
-                    path.append((prev, node))
-                    node = prev
-                path.reverse()
-                return path
-
-            backtracked = False                                       # Flag to check if backtracking occurred
-            for neighbor in graph.neighbors(node):
-                if neighbor not in visited:                           # If the neighbor is not visited , i.e backtracking occurs
-                    stack.append(neighbor)
-                    parent[neighbor] = node
-                    backtracked = True
-            
-            if not backtracked and node in parent:                     # If backtracking occurs, draw the path in purple
-                draw_path([(parent[node], node)], 'purple')
-
-    return []
-
-
-def bfs_traversal(graph,start,goal):
-    visited = set()
-    queue = [start]
-    parent = {}
-    path_set = []
-
-    while queue:
-        node = queue.pop(0)
-
-        if node not in visited:
-            visited.add(node)
-
-            if node in parent:
-                path_set.append((parent[node], node))
-                draw_path([(parent[node], node)],'green')
-
-            if node == goal:
-                return visited,path_set
-            
-            for neighbor in graph.neighbors(node):
-                if neighbor not in visited:
-                    queue.append(neighbor)
-                    parent[neighbor] = node
-
-    return visited, path_set
-
+        next_cell = current_cell.check_neighbors()
+        if next_cell:
+            next_cell.visited = True
+            stack.append(current_cell)
+            remove_walls(current_cell, next_cell)
+            current_cell = next_cell
+        elif stack:
+            current_cell = stack.pop()
+        else:
+            break
+        
+        yield
 
 
 grid_cells = [Cell(x, y) for y in range(rows) for x in range(cols)]
-current_cell = grid_cells[0]
-stack = []
 sc.fill(pygame.Color('darkslategrey'))
+maze_generator = maze_generator_dfs(grid_cells)
 graph_made = False
-visited_edges = []
+# visited_edges = [] ??
 path = []
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            exit()
-
-    # if not graph_made:    
-    [cell.show() for cell in grid_cells]
-    current_cell.visited = True
-    current_cell.current_cell()
-
-    next_cell = current_cell.check_neighbors()
-    if next_cell:
-        next_cell.visited = True
-        stack.append(current_cell)
-        remove_walls(current_cell, next_cell)
-        current_cell = next_cell
-    elif stack:
-        current_cell = stack.pop()
+            exit()    
+    try:
+        next(maze_generator)
+    except StopIteration:
+        pass
 
     if all(cell.visited for cell in grid_cells):
         G = create_graph()
         graph_made = True
 
-            # for edge in G.edges:
-            #     x1, y1 = edge[0]
-            #     x2, y2 = edge[1]
-            #     pygame.draw.line(sc, pygame.Color('blue'), (x1 * TILE + TILE // 2, y1 * TILE + TILE // 2), (x2 * TILE + TILE // 2, y2 * TILE + TILE // 2), 2)
-
     if pygame.key.get_pressed()[pygame.K_SPACE] and graph_made:
         start = (0, 0)
         if ALGORITHM == 'DFS':
-            path = dfs_traversal(G, start, (cols - 1, rows - 1))
+            path = tr.dfs_traversal(G, start, (cols - 1, rows - 1), TILE, DELAY, LINEWIDTH, sc)
         elif ALGORITHM == 'BFS':
-            visited,path = bfs_traversal(G, start, (cols - 1, rows - 1))
+            visited,path = tr.bfs_traversal(G, start, (cols - 1, rows - 1), TILE, DELAY, LINEWIDTH, sc)
         elif ALGORITHM == 'A*':
-            path = a_star(G, start, (cols - 1, rows - 1))
+            path = tr.a_star(G, start, (cols - 1, rows - 1), TILE, DELAY, LINEWIDTH, sc)
 
-    draw_path(path,'orange')
+    tr.draw_path(path,'orange',  TILE, DELAY, LINEWIDTH, sc)
         
     pygame.display.flip()
     clock.tick(TICK)
